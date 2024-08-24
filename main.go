2c7de6d8d30abdd88e87ed8e9a5a72856da2d696
adarsh-jaiss <its.adarshjaiss@gmail.com>
2024-08-24 12:21:04 +0530

    added new code
    Signed-off-by: adarsh-jaiss <its.adarshjaiss@gmail.com>

--- main.go
+++ main.go
@@ -1,26 +1,84 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
-	"encoding/json"
+
+	"strconv"
+
+	"github.com/gofiber/fiber/v3"
 )
 
-func main() {
-	http.HandleFunc("/", getAPI)
-	fmt.Println("Server started at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+type Exercise struct {
+	ID   string
+	Name string
+	Url  string
 }
 
-func getAPI(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+type ExerciseList struct {
+	ID       string
+	ListName string
+	Img      string
+	Details  string
+	Video    string
+}
 
-	response := struct {
-		Message string `json:"msg"`
-	}{
-		Message: "Hello Shipper",
+func main() {
+	app := fiber.New()
+	app.Get("/ex", HandleGetExercises)
+	app.Get("/elist", HandleGetExerciseList)
+
+	log.Fatal(app.Listen(":3000"))
+
+}
+
+func HandleGetExercises(c fiber.Ctx) error {
+	exercise := generateExerciseDummyData()
+	return c.JSON(exercise)
+}
+
+func HandleGetExerciseList(c fiber.Ctx) error {
+	exL := generateExerciseListDummyData()
+	return c.JSON(exL)
+}
+
+func generateRandomExercise(id int) Exercise {
+	idStr := strconv.Itoa(id)
+  
+	ex :=  Exercise{
+		ID:   idStr,               // Serialized ID from 1 to 100
+		Name: "Exercise " + idStr, // Name in the format "exercise X"
+		Url:  "http://google.com", // Static URL
+	}
+    return ex
+}
+
+func generateRandomExerciseList(id int) ExerciseList {
+	idStr := strconv.Itoa(id)
+	exl := ExerciseList{
+		ID:       idStr,                               // Serialized ID from 1 to 100
+		ListName: "Exercise List" + idStr,             // ListName in the format "ExerciseList X"
+		Img:      "http://google.com/img/" + idStr,    // Static base URL with ID appended
+		Details:  "Details for Exercise List " + idStr, // Custom details for each list
+		Video:    "http://google.com/video/" + idStr,  // Static base URL with ID appended
+	}
+    return exl
+}
+
+func generateExerciseDummyData() []Exercise {
+	exercises := make([]Exercise, 100)
+	for i := 1; i < 101; i++ {
+		exercises[i] = generateRandomExercise(i)
+
 	}
 
-	json.NewEncoder(w).Encode(response)
+	return exercises
+}
+
+func generateExerciseListDummyData() []ExerciseList {
+	exerciseLists := make([]ExerciseList, 100)
+	for i := 0; i < 101; i++ {
+		exerciseLists[i] = generateRandomExerciseList(i)
+	}
+
+	return exerciseLists
 }
